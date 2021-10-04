457db30cac0b738801ea765d0e4496909c527b04
Omkar Mokashi <mokashi.omkara@gmail.com>
2021-10-04 16:35:45 +0530

    Added the user activity to issue and get the book from the citylibrary.

--- Singleton-Pattern-Library-Application/UserActivity.cs
+++ Singleton-Pattern-Library-Application/UserActivity.cs
@@ -6,5 +6,16 @@ namespace Singleton_Pattern_Library_Application
 {
     public class UserActivity
     {
+        public string message;
+
+        //Funtion that defines the user activity to issue the book from the library and returns the issued book
+        public Book IssueBookFromCityLibrary(string bookName)
+        {
+            Dictionary<string, dynamic> issuedBook = new Dictionary<string, dynamic>();
+            CityLibrary cityLibrary = CityLibrary.getInstance();
+            issuedBook = cityLibrary.IssueBook(bookName);
+            this.message = issuedBook["Message"];
+            return issuedBook["Book"];
+        }
     }
 }
