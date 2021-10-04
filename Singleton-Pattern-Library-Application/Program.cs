ce102c20566661e153879d92f1264755fabfc405
Omkar Mokashi <mokashi.omkara@gmail.com>
2021-10-04 15:49:30 +0530

    New project template created.

---
+++ Singleton-Pattern-Library-Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton_Pattern_Library_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
